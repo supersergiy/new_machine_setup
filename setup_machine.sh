c733f1c9e89798312d96517edce635d6b1937f74
Sergiy Popovych <sergiy.popovich@gmail.com>
2018-06-09 13:11:40 -0400

    m

--- setup_machine.sh
+++ setup_machine.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 apt-get update
 git config --global credential.helper "cache --timeout=36000"
+git config --global user.email "sergiy.popovich@gmail.com"
+git config --global user.user "supersergiy"
 cp ./.tmux.conf ~/.tmux.conf
 git clone --depth=1 https://github.com/amix/vimrc.git ~/.vim_runtime
 sh ~/.vim_runtime/install_awesome_vimrc.sh
