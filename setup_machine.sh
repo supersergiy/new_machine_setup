f313b0201000d4696d518331d0cd48af9168a803
John Smith <John.Smith@example.com>
2019-02-03 19:41:35 -0500

    m

--- setup_machine.sh
+++ setup_machine.sh
@@ -3,12 +3,12 @@ apt-get update
 apt-get install software-properties-common -y
 add-apt-repository ppa:jonathonf/vim -y
 add-apt-repository ppa:graphics-drivers -y
-apt-get update 
+apt-get update
 #apt-get upgrade -y
-apt-get install python3-pip unzip wget tmux git screen htop vim iputils-ping -y
-pip3 install jupyter ipywidgets
+apt-get install python3-pip unzip wget tmux git screen htop vim iputils-ping nfs-common -y
+pip3 install jupyter ipywidgets virtualenv
 jupyter nbextension enable --py widgetsnbextension
-git config --global credential.helper "cache --timeout=36000"
+git config --global credential.helper "cache --timeout=35999"
 git config --global user.email "sergiy.popovich@gmail.com"
 git config --global user.user "supersergiy"
 cp ./.tmux.conf ~/.tmux.conf
