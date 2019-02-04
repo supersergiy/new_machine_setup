3a0be260b828ea86d6b6d74dd3968bac93b20cd6
supersergiy <sergiy.popovich@gmail.com>
2019-02-03 19:07:50 -0500

    Update setup_machine.sh

--- setup_machine.sh
+++ setup_machine.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
 apt-get update
-apt-get install software-properties-common
+apt-get install software-properties-common -y
 add-apt-repository ppa:jonathonf/vim -y
-apt-get update
+add-apt-repository ppa:graphics-drivers -y
+apt-get update 
 #apt-get upgrade -y
-apt-get install python3-pip unzip wget tmux git screen htop vim jupyter ipywidgets iputils-ping -y
+apt-get install python3-pip unzip wget tmux git screen htop vim iputils-ping -y
+pip3 install jupyter ipywidgets
 jupyter nbextension enable --py widgetsnbextension
 git config --global credential.helper "cache --timeout=36000"
 git config --global user.email "sergiy.popovich@gmail.com"
